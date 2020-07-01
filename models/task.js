33a472b512ac19aee97621726b7e0bf6297e9061
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-01 23:33:39 +0300

    added lots of func for task

--- models/task.js
+++ models/task.js
@@ -1,25 +1,37 @@
 const db = require('../database/db');
 
 module.exports = class Task {
-    constructor(id, title, description, status) {
-        this.id = id;
+    constructor( title, description) {
         this.title = title;
         this.description = description;
-        this.status = status;
     };
 
-/*     save(){
+    save(){
+        return db.execute('INSERT INTO tasks (title, description) VALUES (?, ?)',
+        [this.title, this.description]);
+    };
+    //--------------------------status--------------------------------
+    static setUser(userId){
+        return db.execute('INSERT INTO tasks (userId) VALUES (?)',[userId]);
+    }
 
-    }; */
+    static inProgress(){
+        return db.execute('INSERT INTO tasks (status) VALUES (?)',['In progress']);
+    }
 
+    static done(){
+        return db.execute('INSERT INTO tasks (status) VALUES (?)',['DONE']);
+    }
+    // --------------------------deleting----------------------------
+    static deleteById(id){
+        return db.execute('DELETE * FROM tasks WHERE tasks.id = (?)',[id]);
+    }
+    // ------------------------finding by id------------------------
+   static findById(id){
+        return db.execute('SELECT * FROM tasks WHERE tasks.id = (?)',[id]);
+   }
+   //--------------------------fething all tasks-----------------------
     static fetchAll(){
-        return db.execute('SELECT * FROM tasks')
-        /* .then(result=>{
-            //console.log(result);
-            console.log('success from model');
-        })
-        .catch(err=>{
-            console.log('from model ',err);
-        }) */
+        return db.execute('SELECT * FROM tasks');
     }
 }
