6cf2437f29aae651d4a34d270cc6ed7193a2d2d2
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-03 11:14:02 +0300

    added creator to post + deleting logic

--- controllers/task.js
+++ controllers/task.js
@@ -1,79 +1,127 @@
 const Task = require('../models/task');
+const User = require('../models/user');
 
-exports.getTasks=(req,res,next)=>{
+exports.getTasks = (req, res, next) => {
+    /* const currentPage = req.query.page || 1;
+    const perPage = 5;
+    totalItems = Task.countItems(); */
     Task.fetchAll()
-    .then(result=>{
-        console.log('from controller',result[0]);
-        res.status(200).json({
-            message:"ALL THE TASKS",
-            tasks: result[0]
-        });
-    })
-    .catch(err=>{
-        console.log(err);
-    })
+        .then(result => {
+            console.log('from controller', result[0]);
+            res.status(200).json({
+                message: "ALL THE TASKS",
+                tasks: result[0],
+                /*  totalItems: totalItems */
+            });
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        })
 }
 
-exports.postTasks=(req,res,next)=>{
+exports.postTasks = (req, res, next) => {
     const title = req.body.title
     const description = req.body.description;
-    console.log(title, description);
-    task = new Task( title, description );
+
+    const creator = Number.parseInt(req.userId);
+    task = new Task(title, description, creator);
     console.log(task);
     task.save()
-    .then(result=>{
-        res.status(201).json({
-            message:"task has been successfuly added B)",
-        });
-    })
-    .catch(err=>{
-        console.log(err);
-    })
+        .then(result => {
+            res.status(201).json({
+                message: "task has been successfuly added B)",
+            });
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        })
 };
 
-exports.deleteTask=(req,res,next)=>{
+exports.deleteTask = (req, res, next) => {
     const taskId = req.params.taskId;
     const id = Number.parseInt(taskId);
-    Task.deleteById(id)
-        /* .then(result=>{
-            return  User.findById(req.userId)
+    let worker;
+    Task.findById(id)
+        .then(result => {
+            const task = result[0][0];
+            console.log(task);
+            worker = task.userId;
+            if (worker === null) {
+                return Task.deleteById(id);
+            }
+            else {
+                return User.findById(worker)
+                    .then(result => {
+                        const user = result[0][0];
+                        console.log(user);
+                        user.taskId = null;
+                        return user.save();
+                    })
+                    .then(result => {
+                        return Task.deleteById(id);
+                    })
+            }
         })
-        .then(user=>{
-            user.posts.pull(postId);
-            return user.save();
-        }) */
-        .then(result=>{
-            res.status(200).json({message:'Post deleted successfuly',})
+        .then(result => {
+            res.status(200).json({ message: 'Task deleted successfuly', })
         })
-        .catch(err=>{
-            console.log(err);
+        .catch(err => {
+            res.status(500).send({ message: err.message });
         });
 }
 
-exports.updateTask = (req,res,next)=>{
+exports.updateTask = (req, res, next) => {
     const title = req.body.title
     const description = req.body.description;
     const taskId = req.params.taskId;
     const id = Number.parseInt(taskId);
     Task.findById(id)
-        .then(taskych=>{
+        .then(taskych => {
             status = taskych[0][0].status;
-            if(status === 'view'){
+            if (status === 'view') {
                 return Task.updateById(title, description, id)
-                .then(result=>{
-                    console.log(result);
-                    res.status(200).json({
-                        message:"succesfuly updated!",
+                    .then(result => {
+                        console.log(result);
+                        res.status(200).json({
+                            message: "succesfuly updated!",
+                        })
                     })
+            }
+            else {
+                return res.status(200).json({
+                    message: "failed to update a task, because this task not on the view)"
                 })
             }
-            else{
-                return res.status(200).json({
-                    message:"failed to update a task, because this task not on the view)"
-                })
-            } 
         })
-        .catch(err=>{
-            console.log(err);
+        .catch(err => {
+            res.status(500).send({ message: err.message });
         })
 }
+
+exports.statusInProgress = (req, res, next) => {
+    const taskId = req.params.taskId;
+    const id = Number.parseInt(taskId);
+    Task.inProgress(id)
+        .then(result => {
+            res.status(200).json({
+                message: "status updated to IN PROGRESS"
+            });
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        })
+};
+
+exports.statusDone = (req, res, next) => {
+    const taskId = req.params.taskId;
+    const id = Number.parseInt(taskId);
+    Task.done(id)
+        .then(result => {
+            res.status(200).json({
+                message: "status updated to Done"
+            });
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });;
+        })
+};
