6cf30316ea2f8d1cf179ae38e5c5acb442616a7d
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-03 11:13:30 +0300

    added user routs

--- app.js
+++ app.js
@@ -3,33 +3,38 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-//const isAuth = require('./middleware/is-auth');
+const isAuth = require('./middleware/is-auth');
 
 //const db = require('./database/db');
 
 //================== routes =======================
 const taskRoutes = require('./routes/task.js');
-//const authRoutes = require('./routes/auth.js');
+const userRoutes = require('./routes/user.js');
 //=======================================
 
 app.use(bodyParser.json());     //application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
-    if(req.method === "OPTIONS"){
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === "OPTIONS") {
         return res.sendStatus(200);
     }
     next();
 })
 
-app.use('/task',taskRoutes);
+
+
+app.use('/user', userRoutes);
+
+app.use('/task', taskRoutes);
+
 //db.execute('')
 
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log('success');
 });
