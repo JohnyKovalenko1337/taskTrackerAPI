3de8101a02b072c50621ede91a151e53c169df5a
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-03 12:36:58 +0300

    added dummy validation

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const userController = require('../controllers/user');
 
@@ -6,7 +7,14 @@ const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-router.post('/signup', userController.signup);
+router.post('/signup', [
+    body('email').isEmail().withMessage('Please input valid email'),
+    body('first_name').trim().notEmpty().isString(),
+    body('last_name').trim().notEmpty().isString(),
+    body('password', 'Please enter password with only text and numbers at least 5 symbols').trim()
+        .isLength({ min: 5 }).isAlphanumeric(),
+
+], userController.signup);
 
 router.post('/login', userController.login);
 
@@ -20,5 +28,6 @@ router.put('/update/:userId', isAuth, userController.updateUser);
 
 router.delete('/delete/:userId', isAuth, userController.deleteUser);
 
-//router.
+router.put('/giveTask/:taskId/forUser/:userId', isAuth, userController.giveTask)
+
 module.exports = router;
